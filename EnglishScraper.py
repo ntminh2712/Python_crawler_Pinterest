85a0c28fe2358b2c2d4bb9cb7b540ed30020e9da
minhnt-appart <nguyen_tuan_minh@appart.co.jp>
2020-11-20 01:28:34 +0700

    add code

---
+++ EnglishScraper.py
@@ -0,0 +1,54 @@
+#Import necessary libraries
+from urllib.request import urlretrieve
+import os.path
+import os
+
+
+#Creates a class that contains the scraping essentials
+class ScrapingEssentials(object):
+
+    #Necessaary Class Variables
+    currentItem = 0
+    number = 0
+    categories = []
+    #Initializing a directory for the pictures to come in
+    def __init__(self, source):
+        ScrapingEssentials.number = 0
+        self.source = source
+        file_path_string = "/Users/Admin/Documents/PinterestScraper-master/Crawler/" + source
+        if not os.path.exists(file_path_string):
+            os.makedirs(file_path_string)
+    # Needed to make a class to have a static way of counting
+    def reset(self):
+        ScrapingEssentials.number = 0
+        ScrapingEssentials.currentItem += 1
+    #Convert the file from a url to an actual image file and store it on the commputer
+    def download_image(self, link):
+        try:
+            done = False
+            print("processing file: " + str(ScrapingEssentials.number))
+            #Make a requests object
+            #Make a folder name
+            folder_name = ScrapingEssentials.categories[ScrapingEssentials.currentItem]
+            print(link)
+            #Make the directory of the folder
+            file_path_string = "/Users/Admin/Documents/PinterestScraper-master/Crawler/" + self.source + "/" + folder_name
+            file_path = os.path.join(file_path_string, (str(ScrapingEssentials.number) + ".jpg"))
+
+            if not os.path.exists(file_path_string):
+                os.makedirs(file_path_string)
+            #Download it on the computer
+            print(file_path + "  " + link)
+            ScrapingEssentials.number += 1
+
+            urllib.request.urlretrieve(link, file_path)
+
+            print
+        except Exception:
+            pass
+
+    def english_pickle(self):
+        ScrapingEssentials.categories = [
+            "Gái Xinh"
+        ]
+        return ScrapingEssentials.categories
